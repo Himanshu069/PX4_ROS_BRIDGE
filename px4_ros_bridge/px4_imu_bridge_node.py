6630081cee723b93ecdb79e49513c8a38a2b2600
Himanshu069 <078bme014.himanshu@pcampus.edu.np>
2025-08-01 23:17:54 +0545

    Initial commit of submodule

---
+++ px4_ros_bridge/px4_imu_bridge_node.py
@@ -0,0 +1,51 @@
+import rclpy
+from rclpy.node import Node
+from px4_msgs.msg import SensorCombined
+from sensor_msgs.msg import Imu
+from builtin_interfaces.msg import Time
+from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy
+
+class PX4IMUBridge(Node):
+    def __init__(self):
+        super().__init__('px4_imu_bridge')
+        qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.TRANSIENT_LOCAL,
+            depth=10
+        )
+        self.sub = self.create_subscription(SensorCombined,
+                                            '/px4_1/fmu/out/sensor_combined',
+                                            self.callback, qos_profile )
+        self.pub = self.create_publisher(Imu, '/imu/data', qos_profile)
+
+    def callback(self, msg):
+        imu_msg = Imu()
+        # Timestamp (approximate)
+        now = self.get_clock().now().to_msg()
+        imu_msg.header.stamp = now
+        imu_msg.header.frame_id = "x500_depth_1/OakD-Lite/base_link/StereoOV7251"  # change as needed (e.g. imu_link)
+
+
+        imu_msg.angular_velocity.x = float(msg.gyro_rad[0])
+        imu_msg.angular_velocity.y = float(msg.gyro_rad[1])
+        imu_msg.angular_velocity.z = float(msg.gyro_rad[2])
+
+        imu_msg.linear_acceleration.x = float(msg.accelerometer_m_s2[0])
+        imu_msg.linear_acceleration.y = float(msg.accelerometer_m_s2[1])
+        imu_msg.linear_acceleration.z = float(msg.accelerometer_m_s2[2])
+
+        # No orientation data in SensorCombined → leave orientation unset (NaNs)
+        imu_msg.orientation_covariance[0] = -1  # marks "no orientation estimate"
+
+        self.pub.publish(imu_msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PX4IMUBridge()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
